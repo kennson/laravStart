3048f6f874eab9f2f98c91f5d8d393426b62aa98
kennson <ken_nson@yahoo.com>
2020-03-27 12:14:04 +0800

    add component

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -73,12 +73,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
                with font-awesome or any other icon font library -->
           <li class="nav-item">
           <router-link to="/dashboard" tag="a" class="nav-link"  active-class="active" exact>
-              <i class="nav-icon fas fa-tachometer-alt text-blue"></i>
-              <p>
-                Dashboard
+            <i class="nav-icon fas fa-tachometer-alt text-blue"></i>
+            <p>
+              Dashboard
 
-              </p>
-            </router-link>
+            </p>
+          </router-link>
           </li>
              
           <li class="nav-item has-treeview">
@@ -91,27 +91,22 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Active Page</p>
-                </a>
+                <router-link to="/users" tag="a" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Users</p>
+                </router-link>
               </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Inactive Page</p>
-                </a>
-              </li>
+            
             </ul>
           </li>
 
           <li class="nav-item">
           <router-link to="/profile" tag="a" class="nav-link"  active-class="active" exact>
-              <i class="nav-icon fas fa-user text-orange"></i>
-              <p>
-                Profile
-              </p>
-            </router-link>
+            <i class="nav-icon fas fa-user text-orange"></i>
+            <p>
+              Profile
+            </p>
+          </router-link>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="{{ route('logout') }}"
